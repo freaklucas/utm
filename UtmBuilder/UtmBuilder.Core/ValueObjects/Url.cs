2051b3a95935b13f65254e9971a3bc55a293a952
freaklucas <lucas.oliveira.freak@gmail.com>
2024-10-10 23:14:29 -0300

    init

---
+++ UtmBuilder/UtmBuilder.Core/ValueObjects/Url.cs
@@ -0,0 +1,10 @@
+namespace UtmBuilder.Core.ValueObjects;
+
+public class Url : ValueObject
+{
+    public Url(string address)
+    {
+        Address = address;
+    }
+    public string Address { get; }
+}
