1d7ec88b7b1df41d9b4ae9788effe962e9b88a72
freaklucas <lucas.oliveira.freak@gmail.com>
2024-10-13 18:57:05 -0300

    feat:operador implicito p UTM

--- UtmBuilder/UtmBuilder.Core/Utm.cs
+++ UtmBuilder/UtmBuilder.Core/Utm.cs
@@ -1,4 +1,5 @@
 using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core;
 
@@ -19,4 +20,42 @@ public class Utm
     ///     Campaign Details
     /// </summary>
     public Campaign Campaign { get; }
+
+    public static implicit operator string(Utm utm)
+    {
+        return utm.ToString();
+    }
+
+    public override string ToString()
+    {
+        var segments = new List<string>();
+
+        segments.AddIfNotNull("utm_source", Campaign.Source);
+        segments.AddIfNotNull("utm_medium", Campaign.Medium);
+        segments.AddIfNotNull("utm_campaign", Campaign.Name);
+        segments.AddIfNotNull("utm_id", Campaign.Id ?? string.Empty);
+        segments.AddIfNotNull("utm_term", Campaign.Term ?? string.Empty);
+        segments.AddIfNotNull("utm_content", Campaign.Content ?? string.Empty);
+
+        return $"{Url.Address}?{string.Join("&", segments)}";
+    }
+
+    /* https://lucasol.vercel.app
+    utm_source=lucasol
+    utm_medium=blog&
+    utm_campaign=LUC10OFF
+    utm_id=LC&
+    utm_content=post-sobre-carreira */
+}
+
+public class X
+{
+    public void Y()
+    {
+        var campaign = new Campaign("lucasol", "blog", "campanha do blog");
+        var url = new Url("www.lucas.com");
+        var utm = new Utm(url, campaign);
+
+        string resultado = utm;
+    }
 }
---
+++ UtmBuilder/UtmBuilder.Core/ValueObjects/Exceptions/ListExtensions.cs
@@ -0,0 +1,14 @@
+namespace UtmBuilder.Core.ValueObjects.Exceptions;
+
+public static class ListExtensions
+{
+    
+    public static void AddIfNotNull(
+        this List<string> list, 
+        string key, 
+        string? value)
+    {
+       if(!string.IsNullOrEmpty(value)) 
+           list.Add($"{key}={value}");
+    }
+}
